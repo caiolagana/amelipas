412047ea25d77964f8005fda386eefe76478387a
Caio Laganá Fernandes <caiolagana@gmail.com>
2025-10-22 14:23:56 -0300

    melhorando home

--- web/front/src/app/app.routes.ts
+++ web/front/src/app/app.routes.ts
@@ -6,11 +6,10 @@ import { Contratos } from './pages/contratos/contratos';
 import { Membros } from './pages/membros/membros';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: Home },
+  { path: '', component: Home },
   { path: 'estatuto', component: Estatuto },
   { path: 'contato', component: Contato },
   { path: 'contratos', component: Contratos },
   { path: 'membros', component: Membros },
-  { path: '**', redirectTo: '/home' } // Wildcard route for 404 pages
+  { path: '**', redirectTo: '/' } // Wildcard route for 404 pages
 ];
